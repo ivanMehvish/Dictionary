744b92353fee6f78f70bc3b6c74f111df05ebe27
Mehvish Fatima <87938552+ivanMehvish@users.noreply.github.com>
2024-10-15 21:09:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,157 +1,102 @@
-let b1=document.getElementById("b1")
-b1.style.opacity=0;
-var srchbtn=document.getElementById("srchbtn");
-srchbtn.addEventListener("click",valueFetch);
-var wd=document.getElementById("word");
-var audi=document.getElementById("audio");
-var meaning=document.getElementById("meaning");
-var error=document.getElementById("err");
+let b1 = document.getElementById("b1");
+b1.style.opacity = 0;
+var srchbtn = document.getElementById("srchbtn");
+srchbtn.addEventListener("click", valueFetch);
+var wd = document.getElementById("word");
+var audi = document.getElementById("audio");
+var meaning = document.getElementById("meaning");
+var error = document.getElementById("err");
 var sound;
 var s_url;
-var list= document.getElementById("mean");
-var phonetic=document.getElementById("phonetic");
-//typewriting
-var x = 0;
+var list = document.getElementById("mean");
+var phonetic = document.getElementById("phonetic");
+var divel = document.getElementById("typewrite");
+
 var txt = 'For all your Language doubts';
-var speed = 5;
-var divel=document.getElementById("demo");
+var speed = 5; // Speed of typewriting
 var messageArray;
-//
-//fetches from input box and resets divs, calls getapi()
 
-function valueFetch(){
-srchbtn.removeEventListener("click",valueFetch);
-b1.style.opacity=100;
-list.innerText="";
-phonetic.innerText="";
-error.innerText="";
- var word=f1.v.value;
- wd.innerText=word;
- var url_api=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
- getapi(url_api);
- 
+function valueFetch(event) {
+    event.preventDefault();  // Prevent form submission from reloading the page
+    srchbtn.removeEventListener("click", valueFetch);
+    b1.style.opacity = 1;
+    list.innerText = "";
+    phonetic.innerText = "";
+    error.innerText = "";
+    var word = f1.v.value;
+    wd.innerText = word;
+    var url_api = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    getapi(url_api);
 }
 
-//
-async function show(data){
- 
-   if(data.message!="" && data.message!=undefined)
-   {
-
-        txt=data.message;
-        divel=error;
-        typeWriter();
-        srchbtn.addEventListener("click",valueFetch);
+// Display data from the API
+async function show(data) {
+    if (data.message && data.message !== "") {
+        error.innerText = data.message;
+        srchbtn.addEventListener("click", valueFetch);
         return;
-   }
-  
-    // meaning.innerText=data[0].meanings[0].definitions[0].definition;
-    // document.getElementById("pos").innerText=data[0].meanings[0].partOfSpeech;
-    // var s_url=data[0].phonetics[0].audio;
-    // sound=new Audio(s_url); 
-    let phon=data[0].phonetics.length;
-    for(g=0; g<phon; ++g)
-    {
-        if(data[0].phonetics[g].text!=undefined)
-        {
-            let p1=document.createElement("div");
-            p1.innerText=data[0].phonetics[g].text;
-            // console.log(data[0].phonetics[g].text);
-            phonetic.appendChild(p1);
+    }
+
+    // Fetch phonetics
+    for (let i = 0; i < data[0].phonetics.length; ++i) {
+        if (data[0].phonetics[i].text) {
+            let phoneticText = document.createElement("div");
+            phoneticText.innerText = data[0].phonetics[i].text;
+            phoneticText.classList.add('text-gray-600');
+            phonetic.appendChild(phoneticText);
             break;
         }
     }
-    for(g=0; g<phon; ++g)
-    {
-        if(data[0].phonetics[g].audio!=undefined && data[0].phonetics[g].audio!="")
-        {
-            s_url=data[0].phonetics[g].audio;
-            sound=new Audio(s_url);
+
+    // Fetch audio
+    for (let i = 0; i < data[0].phonetics.length; ++i) {
+        if (data[0].phonetics[i].audio) {
+            s_url = data[0].phonetics[i].audio;
+            sound = new Audio(s_url);
             break;
         }
     }
-    audi.addEventListener("click",Splay);
-    let m=data[0].meanings.length;
-  for(j=0; j<m; ++j){
+    audi.addEventListener("click", Splay);
 
-     
-    let n=data[0].meanings[j].definitions.length;
-    let p=document.createElement("div");
-    p.classList.add('font-bold');
-    p.innerText=data[0].meanings[j].partOfSpeech;
-    list.appendChild(p);
+    // Fetch meanings and add to list
+    for (let j = 0; j < data[0].meanings.length; ++j) {
+        let pos = document.createElement("div");
+        pos.classList.add('font-bold', 'text-lg', 'text-gray-800');
+        pos.innerText = data[0].meanings[j].partOfSpeech;
+        list.appendChild(pos);
 
-    messageArray=data[0].meanings[j].definitions;
-    
-        for(v=0; v<messageArray.length; v++)
-        {
-          let li=document.createElement("div")
-        //   li.setAttribute("id",`${v}`);
-          list.appendChild(li);
-        //   op=document.getElementById(`${v}`);
-          // console.log(li);
-         textposition=0;
-        await typewriter2(v,messageArray,li);
-        }  
- 
- }
-
-
-srchbtn.addEventListener("click",valueFetch);
+        messageArray = data[0].meanings[j].definitions;
+        for (let k = 0; k < messageArray.length; k++) {
+            let defElement = document.createElement("div");
+            defElement.classList.add('text-gray-700', 'pl-4');
+            list.appendChild(defElement);
+            await typewriter2(messageArray[k].definition, defElement);  // Pass the definition text and element
+        }
+    }
+    srchbtn.addEventListener("click", valueFetch);
 }
 
-function Splay(){
-    sound.play();
-    
+function Splay() {
+    if (sound) sound.play();
 }
 
-
-//function to fetch from api.
-async function getapi(url_api){
-    sound="";
-     // Storing response
+// Fetch data from the API
+async function getapi(url_api) {
+    sound = "";
     const response = await fetch(url_api);
-
-    // Storing data in form of JSON
-    var data = await response.json();
-
+    const data = await response.json();
     console.log(data);
-    
     show(data);
-    
- 
-   
 }
 
-function typeWriter() {
-    if (x < txt.length) {
-      divel.innerHTML += txt.charAt(x);
-      x++;
-      setTimeout(typeWriter, speed);
+// Typewriter effect for definitions
+async function typewriter2(text, element) {
+    let currentPosition = 0;
+    element.innerHTML = "";  // Clear previous content
+    element.innerHTML += '*';
+    while (currentPosition < text.length) {
+        element.innerHTML += text.charAt(currentPosition);
+        currentPosition++;
+        await new Promise(resolve => setTimeout(resolve, speed));
     }
-    if(x==txt.length) return;
 }
-
-async function typewriter2(i,messageArray,k) {
-    let count=0;
-    var def=`• `+ messageArray[i].definition;
-    k.innerHTML=def.substring(0,textposition)+" ";
-    let currentPosition=0;
-
-  while (currentPosition <= def.length) {
-  k.innerHTML = def.substring(0, currentPosition) + " ";
-  await new Promise(resolve => setTimeout(resolve, speed));
-  currentPosition++;
-}
-}
-
-
-
-// typeWriter();
-
-
-// fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-
-//API
-
-
